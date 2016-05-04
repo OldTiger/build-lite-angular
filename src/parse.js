5e1794b56228e77e1cb5da2383800e779aa6e2ab
gongdadong <411298820@qq.com>
2016-05-04 09:15:35 +0800

    page 183

--- src/parse.js
+++ src/parse.js
@@ -1,4 +1,8 @@
 'use strict';
+var ESCAPES = {
+    'n': '\n', 'f': '\f', 'r': '\r', 't': '\t',
+    'v': '\v', '\'': '\'', '"': '"'
+};
 function Lexer() {
 
 }
@@ -23,28 +27,39 @@ Lexer.prototype.lex = function (text) {
         if (this.isNumber(this.ch) ||
             (this.ch === '.' && this.isNumber(this.peek()))) {
             this.readNumber();
-        } else if (this.ch === '\''||this.ch ==='"') {
-            this.readString();
+        } else if (this.ch === '\'' || this.ch === '"') {
+            this.readString(this.ch);
         } else {
             throw  'Unexpected next character:' + this.ch;
         }
     }
     return this.tokens;
 };
-Lexer.prototype.readString = function () {
+Lexer.prototype.readString = function (quote) {
     this.index++;
-    var string ='';
-    while(this.index< this.text.length){
+    var string = '';
+    var escape = false;
+    while (this.index < this.text.length) {
         var ch = this.text.charAt(this.index);
-        if(ch ==='\''||ch ==='"'){
+        if (escape) {
+            var replacement = ESCAPES[ch];
+            if (replacement) {
+                string += replacement;
+            } else {
+                string += ch;
+            }
+            escape = false;
+        } else if (ch === quote) {
             this.index++;
             this.tokens.push({
-                text:string,
-                value:string
+                text: string,
+                value: string
             });
             return;
-        }else{
-            string+=ch;
+        } else if (ch === '\\') {
+            escape = true;
+        } else {
+            string += ch;
         }
         this.index++;
     }
@@ -124,11 +139,16 @@ ASTCompiler.prototype.recurse = function (ast) {
     }
 };
 ASTCompiler.prototype.escape = function (value) {
-    if(_.isString(value)){
-        return '\'' + value + '\'';
-    }else{
+    if (_.isString(value)) {
+        return '\'' + value.replace(this.stringEscapeRegex, this.stringEscapeFn) + '\'';
+    } else {
         return value;
     }
+};
+ASTCompiler.prototype.stringEscapeRegex = /[^ a-zA-Z0-9]/g;
+
+ASTCompiler.prototype.stringEscapeFn = function (c) {
+    return '\\u' + ( '0000' + c.charCodeAt(0).toString(16)).slice(-4);
 };
 //ASTCompiler end
 
--- test/scope_spec.js
+++ test/scope_spec.js
@@ -1460,7 +1460,17 @@ describe("Scope", function () {
             scope.$emit('myEvent');
             expect(listener).not.toHaveBeenCalled();
         });
-
+        it("will not parse a string with mismatching quotes", function() {
+            expect(function() { parse( '"abc\''  ); }).toThrow();
+        });
+        it("can parse a string with single quotes inside", function () {
+            var fn = parse("'a\\\'b'");
+            expect(fn()).toEqual('a\'b');
+        });
+        it('can parse a string with double quotes inside', function () {
+            var fn = parse("'a\\\"b'");
+            expect(fn()).toEqual('a\"b');
+        })
     });
 
 });
